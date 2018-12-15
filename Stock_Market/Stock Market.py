2cc20f2283d0d31251c887bec2b8baf6c1f20a1f
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2018-12-15 06:25:42 +0000

    A small stock market data scrap.
    You run it and get the data of the current stock market which is stored in a csv file.

---
+++ Stock_Market/Stock Market.py
@@ -0,0 +1,70 @@
+
+# coding: utf-8
+
+# In[88]:
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas
+
+r=requests.get("https://www.nasdaq.com/markets/indices/major-indices.aspx")
+c=r.content
+soup=BeautifulSoup(c,"html.parser")
+
+all1=soup.find_all("table",{"class":"USMN_MarketIndices"})[0]
+
+
+td=all1.find_all("td")
+l=[]
+#symbols
+for i in range(0,157,6):
+    d={}
+#Change net%
+
+    try:
+        d["Change net%"]=td[i+3].text
+    except:
+        pass
+
+#High
+
+    try:
+        d["High"]=td[i+4].text
+    except:
+        pass
+
+#Low
+
+    try:
+        d["Low"]=td[i+5].text
+    except:
+        pass
+#index value
+
+    try:
+        d["Index"]=td[i+2].text
+    except:
+        pass
+#NAMES
+
+    try:
+        d["Name"]=td[i+1].text
+    except:
+        pass
+
+#Symbol    
+    try:
+            d["Symbol"]=td[i].find("h3").text
+    except:
+            pass
+
+    
+    l.append(d)
+
+
+
+
+df=pandas.DataFrame(l)
+df.to_csv("Output.csv")
+
